9468233d4101382f991a42b06abb41b3d394b53c
Szymon Staron <szymon97staron@gmail.com>
2021-04-13 13:08:51 +0200

    testing js querySelector

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ const age = "32";
 
 console.log(`Cześć nazywam się ${name} i mam ${age} lata`);
 console.log("HEJ");
+
+const emptyParagraph = document.querySelector('.article__paragraph--js');
+
+emptyParagraph.innerHTML = "Cześć dodałem to js ";
+
+
+
+const emptyParagraph2 = document.querySelector('.article__paragraph2--js');
+
+emptyParagraph2.innerHTML = "Teraz robie test TEST";
